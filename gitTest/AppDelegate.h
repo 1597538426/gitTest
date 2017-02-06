da58f683a0c8646b4b1e9203d38d123d823a81b7
周智伟 <zhouzhiwei@zhouzhiweidemac-mini.local>
2017-02-06 17:39:17 +0800

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by 周智伟 on 17/2/6.
+//  Copyright © 2017年 周智伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by 周智伟 on 17/2/6.
+//  Copyright © 2017年 周智伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
