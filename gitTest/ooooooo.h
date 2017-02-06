40873704e7a4a5e6413c178b83a56b8a49f3f769
周智伟 <zhouzhiwei@zhouzhiweidemac-mini.local>
2017-02-06 17:52:42 +0800

    wweewe

---
+++ gitTest/ooooooo.h
@@ -0,0 +1,13 @@
+//
+//  ooooooo.h
+//  gitTest
+//
+//  Created by 周智伟 on 17/2/6.
+//  Copyright © 2017年 周智伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ooooooo : NSObject
+@property(nonatomic,assign)NSInteger iddddd;
+@end
---
+++ gitTest/ooooooo.m
@@ -0,0 +1,13 @@
+//
+//  ooooooo.m
+//  gitTest
+//
+//  Created by 周智伟 on 17/2/6.
+//  Copyright © 2017年 周智伟. All rights reserved.
+//
+
+#import "ooooooo.h"
+
+@implementation ooooooo
+
+@end
